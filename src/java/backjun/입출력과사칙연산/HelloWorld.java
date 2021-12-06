a359bf0046bae215aae0d7f96f58d5e73c6cb697
jeongkuma <toto8948@naver.com>
2021-12-06 18:01:40 +0900

    Initialize

---
+++ src/java/backjun/입출력과사칙연산/HelloWorld.java
@@ -0,0 +1,7 @@
+package java.backjun.입출력과사칙연산;
+
+public class HelloWorld {
+    public void helloWorld() {
+        System.out.print("Hello World");
+    }
+}
---
+++ src/java/programmers/lv1/로또의최고순위와최저순위.java
@@ -0,0 +1,4 @@
+package java.programmers.lv1;
+
+public class 로또의최고순위와최저순위 {
+}
