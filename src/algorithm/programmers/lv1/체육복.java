e5dde84dd27693785d254f3d2a9a61f81b8a7950
jeongkuma <toto8948@naver.com>
2021-12-16 16:36:40 +0900

    LV1     :   체육복 POINT   :   1035(진행 중)

--- src/algorithm/programmers/lv1/체육복.java
+++ src/algorithm/programmers/lv1/체육복.java
@@ -1,14 +1,27 @@
 package algorithm.programmers.lv1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
 public class 체육복 {
     public static void main(String[] args) {
 
-        practice(5, new int[]{2, 4}, new int[]{3});
+//        practice(5, new int[]{2, 4}, new int[]{1, 3, 5});
+//        practice(5, new int[]{2, 4}, new int[]{3});
+//        practice(3, new int[]{3}, new int[]{1});
+//        practice(5, new int[]{2, 4}, new int[]{5, 3});
+//        practice(3, new int[]{1, 2}, new int[]{2, 3});
+        practice(5, new int[]{2, 3, 4}, new int[]{1, 2, 3});
+//        Random ran = new Random();
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println(ran.nextInt(8));
+//        }
     }
 
     /**
      *[탐욕법]
-     * 
+     *
      * 점심시간에 도둑이 들어, 일부 학생이 체육복을 도난당했습니다. 다행히 여벌 체육복이 있는 학생이 이들에게 체육복을 빌려주려 합니다. 학생들의 번호는 체격 순으로 매겨져 있어, 바로 앞번호의 학생이나
      * 바로 뒷번호의 학생에게만 체육복을 빌려줄 수 있습니다. 예를 들어, 4번 학생은 3번 학생이나 5번 학생에게만 체육복을 빌려줄 수 있습니다. 체육복이 없으면 수업을 들을 수 없기 때문에 체육복을
      * 적절히 빌려 최대한 많은 학생이 체육수업을 들어야 합니다.
@@ -35,17 +48,108 @@ public class 체육복 {
      * 3번 학생이 2번 학생이나 4번 학생에게 체육복을 빌려주면 학생 4명이 체육수업을 들을 수 있습니다.
      */
     public static int practice(int n, int[] lost, int[] reserve) {
-        int answer = 0;
+        int answer = n - lost.length;
 
-        for (int i = 0; i < n; i++) {
-            
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+
+
+        // 전체 학생 Loop
+        for (int i = 1; i <= n; i++) {
+            // 분실 학생 Loop
+            for (int j = 0; j < lost.length; j++) {
+                // 분실 학생일 경우
+                if (i == lost[j]) {
+                    // 여분 학생 Loop
+                    for (int k = 0; k < reserve.length; k++) {
+                        // 분실 학생의 앞뒤로 여분 학생이 있는 경우
+                        if (lost[j] - 1 == reserve[k] || lost[j] + 1 == reserve[k]) {
+                            // 여분 학생 빌리기 완료 처리
+                            reserve[k] = 0;
+                            answer++;
+                            break;
+                        }
+                    }
+                }
+            }
 
         }
 
+
+        System.out.println("answer   ::  " + answer);
         return answer;
+
+//        int answer = n - lost.length;
+//
+//        Arrays.sort(lost);
+//        Arrays.sort(reserve);
+//
+//        ArrayList<Integer> losts = new ArrayList<>();
+//
+//        for (int i : lost) {
+//            boolean chk = true;
+//            for (int j : reserve) {
+//                if (i == j) {
+//                    chk = false;
+//                }
+//            }
+//            if (chk) losts.add(i);
+//        }
+//
+//        // 전체 학생 Loop
+//        for (int i = 1; i <= n; i++) {
+//            // 분실 학생 Loop
+//            for (int j = 0; j < losts.size(); j++) {
+//                // 분실 학생일 경우
+//                if (i == losts.get(j)) {
+//                    // 여분 학생 Loop
+//                    for (int k = 0; k < reserve.length; k++) {
+//                        // 분실 학생의 앞뒤로 여분 학생이 있는 경우
+//                        if (losts.get(j) - 1 == reserve[k] || losts.get(j) + 1 == reserve[k]) {
+//                            // 여분 학생 빌리기 완료 처리
+//                            reserve[k] = -1;
+//                            answer++;
+//                            break;
+//                        }
+//                    }
+//                }
+//            }
+//
+//        }
+//
+//
+//        System.out.println("answer   ::  " + answer);
+//        return answer;
     }
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
+        int getCnt = 0;
+
+        // 전체 학생 Loop
+        for (int i = 1; i <= n; i++) {
+            // 분실 학생 Loop
+            for (int j = 0; j < lost.length; j++) {
+                // 분실 학생일 경우
+                if (i == lost[j]) {
+                    // 빌리기 가능 여부 변수 선언
+                    // 여분 학생 Loop
+                    for (int k = 0; k < reserve.length; k++) {
+                        // 분실 학생의 앞뒤로 여분 학생이 있는 경우 && 여분, 분실 학생이 같지 않을 경우
+//                        if ((lost[j] - 1 == reserve[k] || lost[j] + 1 == reserve[k]) && lost[j] != reserve[k]) {
+                        if ((lost[j] - 1 == reserve[k] || lost[j] + 1 == reserve[k]) && lost[j] != reserve[k]) {
+                            // 여분 학생 빌리기 완료 처리
+                            reserve[k] = 0;
+                            getCnt++;
+                            break;
+                        }
+                    }
+
+                }
+            }
+
+        }
+
+        answer = n - lost.length + getCnt;
         return answer;
     }
 
