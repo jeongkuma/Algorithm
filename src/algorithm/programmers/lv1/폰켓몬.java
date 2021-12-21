9074bdb640ea8af89a2326ee0f1eddb90f79beed
jeongkuma <toto8948@naver.com>
2021-12-21 14:50:04 +0900

    LV1     :   예산 POINT   :   1067(+8)

--- src/algorithm/programmers/lv1/폰켓몬.java
+++ src/algorithm/programmers/lv1/폰켓몬.java
@@ -125,7 +125,16 @@ public class 폰켓몬 {
     }
 
     public int solution(int[] nums) {
-        int answer = 0;
-        return answer;
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        if (nums.length / 2 <= set.size()) {
+            return nums.length / 2;
+        } else {
+            return set.size();
+        }
     }
 }
