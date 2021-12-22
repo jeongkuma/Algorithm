03d538ff7cd3ab5718c32f658d7a1975d08e9bd7
jeongkuma <toto894828@gmail.com>
2021-12-22 10:40:21 +0900

    NAME		: 내적 LV			: 1 POINT		: 1076(+1)

--- src/algorithm/programmers/lv1/내적.java
+++ src/algorithm/programmers/lv1/내적.java
@@ -22,7 +22,24 @@ package algorithm.programmers.lv1;
  */
 public class 내적 {
     public static void main(String[] args) {
+        practice(new int[]{1,2,3,4}, new int[]{-3,-1,0,2});
+        practice(new int[]{-1, 0, 1}, new int[]{1, 0, -1});
+    }
 
+    public static int practice(int[] a, int[] b) {
+        int answer = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+
+        System.out.println(answer);
+        return answer;
+    }
+
+    public int solution(int[] a, int[] b) {
+        int answer = 1234567890;
+        return answer;
     }
 
 }
