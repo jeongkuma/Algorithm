24ed444477b294b94b35c0ac81bbe8d731231778
jeongkuma <toto894828@gmail.com>
2022-01-03 13:45:24 +0900

    NAME		: 프린터 LV			: 2 POINT		: 1137(Sync)

--- src/algorithm/programmers/lv2/프린터.java
+++ src/algorithm/programmers/lv2/프린터.java
@@ -33,4 +33,7 @@ package algorithm.programmers.lv2;
  * 6개의 문서(A, B, C, D, E, F)가 인쇄 대기목록에 있고 중요도가 1 1 9 1 1 1 이므로 C D E F A B 순으로 인쇄합니다.
  */
 public class 프린터 {
+    public static void main(String[] args) {
+
+    }
 }
