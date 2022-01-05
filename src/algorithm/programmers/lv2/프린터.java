19e262ce6878288fa3d218d8c5b54ae7b9874cd2
jeongkuma <toto894828@gmail.com>
2022-01-05 14:30:01 +0900

    NAME		: 프린터 LV			: 2 POINT		: 1137(템플릿)

--- src/algorithm/programmers/lv2/프린터.java
+++ src/algorithm/programmers/lv2/프린터.java
@@ -2,6 +2,9 @@ package algorithm.programmers.lv2;
 
 
 /**
+ *
+ * stack
+ *
  * 일반적인 프린터는 인쇄 요청이 들어온 순서대로 인쇄합니다. 그렇기 때문에 중요한 문서가 나중에 인쇄될 수 있습니다. 이런 문제를 보완하기 위해 중요도가 높은 문서를 먼저 인쇄하는 프린터를 개발했습니다. 이
  * 새롭게 개발한 프린터는 아래와 같은 방식으로 인쇄 작업을 수행합니다.
  *
@@ -34,7 +37,9 @@ package algorithm.programmers.lv2;
  */
 public class 프린터 {
     public static void main(String[] args) {
-
+        practice(new int[]{2, 1, 3, 2}, 2);
+//        practice(new int[]{1, 1, 9, 1, 1, 1}, 0);
+//        practice(new int[]{2, 1, 3, 2}, 2);
     }
 
     public static int practice(int[] priorities, int location) {
