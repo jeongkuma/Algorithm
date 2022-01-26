125ce934fb72c5ef59cbc01cfe996943d0c17bd0
jeongkuma <toto894828@gmail.com>
2022-01-26 11:11:34 +0900

    NAME		: StringUtil Sample LV			: 2 POINT		: 1137(none)

---
+++ src/util/compareString.java
@@ -0,0 +1,40 @@
+package util;
+
+import java.util.Locale;
+
+public class compareString {
+
+    public static void main(String[] args) {
+        compare("VTS", "VTS", true, true);
+    }
+
+    public static boolean compare(String left, String right, boolean caseSensitive, boolean equals) {
+        int iRv = 0;
+        boolean bRv = true;
+
+        if (right != null) {
+            if (left != null) {
+                if (caseSensitive) {
+                    iRv = right.compareTo(left);
+                } else {
+                    iRv = right.compareTo(left.toLowerCase());
+                }
+            }
+        }
+
+        if (equals) {
+            if (iRv < 0) {
+                bRv = false;
+            }
+        } else {
+            if (iRv <= 0) {
+                bRv = false;
+            }
+        }
+
+
+        System.out.println("bRv ::  " + bRv);
+        return bRv;
+
+    }
+}
