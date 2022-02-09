4c06f710abd4bdf4b2ca008fc43744c5c529c46b
jeongkuma <toto894828@gmail.com>
2022-02-09 10:26:38 +0900

    NAME		: StringUtil Sample LV			: 2 POINT		: 1137(none)

--- src/algorithm/programmers/skillCheck/Level1.java
+++ src/algorithm/programmers/skillCheck/Level1.java
@@ -2,6 +2,8 @@ package algorithm.programmers.skillCheck;
 
 
 /**
+ * skill chek 정답 찾아서 해보자..
+ *
  * 임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
  * n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
  *
